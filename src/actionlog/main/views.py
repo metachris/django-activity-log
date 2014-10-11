26b35261b437029db794c0b20c276f464fb2c767
Chris Hager <chris.hager@bitsworking.com>
2014-10-11 15:59:22 +0200

    base template

--- src/actionlog/main/views.py
+++ src/actionlog/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from traceback import format_exc
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.generic.base import View
+from django.shortcuts import render_to_response, render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.contrib import auth
+from django.core.urlresolvers import reverse_lazy
+from django.conf import settings
+
+from main.models import *
+
+import datetime
+import time
+from pprint import pprint, pformat
+
+def index(request):
+    return render(request, 'index.html')
